62364066116c4e2614abe3f774600c61d60a0ad4
Ygarciaa <ygarcia@estudiante.ceibal.edu.uy>
2025-07-07 09:42:51 -0300

    El index de los residentes, que ni diseño tiene .

---
+++ resources/views/index.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <div class="solicitudes">
+            <h2>Solicitudes</h2>
+            <ul>
+                <li><a href="Formularios.blade.php#1">Juan</a></li>
+                <li><a href="Formularios.blade.php#2">Fulano</a></li>
+                <li><a href="Formularios.blade.php#3">Pepe</a></li>
+                <li><a href="Formularios.blade.php#4">Lusmiglio</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <h2>Avisos Legales</h2>
+            <div class="Mision">
+                <li><a href="AvisosLegales.blade.php#1"></a></li>
+                <li><a href="AvisosLegales.blade.php#2"></a></li>
+                <li><a href="AvisosLegales.blade.php#3"></a></li>
+            </div>
+        </div>
+    </main>
+
+    <aside>
+        <h2>Novedades</h2>
+        <div class="Residentes">
+            <ul>
+                <li><a href="Datosdelosresidentes.blade.php#1"></a></li>
+                <li><a href="Datosdelosresidente.blade.php#2"></a></li>
+                <li><a href="Datosdelosresidente.blade.php#3"></a></li>
+            </ul>
+        </div>
+
+        <h2>Avisos Legales</h2>
+        <div class="AvisosLegales">
+            <ul>
+                <li><a href="AvisosLegales.blade.php#1"></a></li>
+                <li><a href="AvisosLegales.blade.php#2"></a></li>
+                <li><a href="AvisosLegales.blade.php#3"></a></li>
+            </ul>
+        </div>
+
+        <h2>Configuración</h2>
+        <div class="Configuracion">
+            <ul>
+                <li><a href="configuracion.blade.php#1"></a></li>
+                <li><a href="configuracion.blade.php#2"></a></li>
+                <li><a href="configuracion.blade.php#3"></a></li>
+            </ul>
+        </div>
+    </aside>
+</body>
+</html>
