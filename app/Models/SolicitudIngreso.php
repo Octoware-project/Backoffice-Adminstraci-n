190b2669706e6deac0d484dee9e0603609c156ff
BellCramwelll <martinripoll448@gmail.com>
2025-07-10 15:00:36 -0300

    creacion de modelo de solicitud

---
+++ app/Models/SolicitudIngreso.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SolicitudCooperativa extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'Nombre_Completo',
+        'Cedula',
+        'Celular',
+        'Fecha_nacimiento',
+        'Correo',
+        'Nacionalidad',
+        'Estado_civil',
+        'Ingresos_totales',
+        'Estado',
+    ];
+}
