301522d739cf13ae977317e29e6bebc5a93eedb0
BellCramwelll <martinripoll448@gmail.com>
2025-07-10 15:25:58 -0300

    actualize las credenciales

--- app/Models/SolicitudRegistro.php
+++ app/Models/SolicitudRegistro.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SolicitudCooperativa extends Model
+class SolicitudRegistro extends Model
 {
     use HasFactory;
 
