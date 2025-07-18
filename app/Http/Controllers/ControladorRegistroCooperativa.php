09e13e43b061a8b6e6f0190fdd16ab0e352b9c1a
BellCramwelll <martinripoll448@gmail.com>
2025-07-17 21:53:25 -0300

    agrege la aceptacion del registro y la baja logica

--- app/Http/Controllers/ControladorRegistroCooperativa.php
+++ app/Http/Controllers/ControladorRegistroCooperativa.php
@@ -8,8 +8,20 @@ class ControladorRegistroCooperativa extends Controller
 {    
 public function recibir()
     {
-        $personas = Persona::where('Estado', 'pendiente')->get()->toArray();
-        
+    $personas = Persona::where('Estado', 'pendiente')->get()->toArray();   
     return view('persona', ['personas' => $personas]);
+    }
+    
+    public function AceptarPersona($id)
+    {
+    $persona = persona::findOrFail($id);
+    $persona->Estado = 'Aceptado';
+    $persona->save();
+    }
+    
+    public function RechazarPersona($id){
+    $persona = persona::findOrFail($id);
+    $persona->Estado = 'Rechazado';    
+    $persona->delete();
     }
 }
